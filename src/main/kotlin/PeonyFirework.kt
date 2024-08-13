9b25d3bcbe52fb6c40c75a8d5e992f07de633149
Marcin Kienast <kienastmarcin@gmail.com>
2024-08-13 23:12:36 +0200

    NO-TASK: Use Verlet integration to calculate motion

--- src/main/kotlin/PeonyFirework.kt
+++ src/main/kotlin/PeonyFirework.kt
@@ -3,25 +3,26 @@ import org.openrndr.math.Vector2
 
 class PeonyFirework(initialX: Double, initialY: Double, private var fuel: Double) : Particle() {
     private var pos: Vector2 = Vector2(initialX, initialY)
-    private var velocity: Vector2 = Vector2(1.0)
-
+    private var velocity: Vector2 = Vector2(0.0)
+    private var acceleration: Vector2 = Vector2(0.0)
     private var emitted = false
 
     override fun update(deltaSeconds: Double): Collection<Particle> {
         val mass = 1.5;
         val burnRate = 120.0;
-
         val gravity = Vector2(0.0, 1000.0)
+
         val thrust =
             if (fuel > 0.0) Vector2(0.0, -2200.0)
             else Vector2(0.0)
         fuel -= burnRate * deltaSeconds
-
         val force = thrust + gravity
-        val acceleration = force / mass
-        velocity += acceleration * deltaSeconds
-        val deltaPosition = velocity * deltaSeconds
+
+        val deltaPosition = velocity * deltaSeconds + acceleration * (deltaSeconds * deltaSeconds * 0.5)
         pos += deltaPosition
+        val newAcceleration = force / mass
+        velocity += (acceleration + newAcceleration) * (deltaSeconds * 0.5)
+        acceleration = newAcceleration
 
         if (fuel <= 0.0 && deltaPosition.y >= 0.0 && !emitted) {
             emitted = true
